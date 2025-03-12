0a3d80fb109c48ff96d1f48d2733357d71dd4741
Abaseen Khan <arbaseen@gmail.com>
2025-03-11 20:40:01 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, request
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import TextFormatter
+
+app = Flask(__name__)
+
+@app.route('/sub', methods=['GET'])
+def sub():
+    video_id = request.args.get('video_id')
+
+    try:
+        subtitles = YouTubeTranscriptApi.get_transcript(video_id)
+        formatter = TextFormatter()
+        formatted_subtitles = formatter.format_transcript(subtitles)
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
